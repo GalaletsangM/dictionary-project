b6fdce9ba93f061d1265bdc055eaff9a9b3c460a
GalaletsangM <preciousmafoko@gmail.com>
2024-11-19 11:55:09 -0600

    fixed the warning on the Phonetics component

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -4,7 +4,7 @@ import "./Phonetics.css"
 export default function Phonetics(props){
     return (
       <div className="Phonetics">
-        <a href={props.phonetic.audio} target="_blank">
+        <a href={props.phonetic.audio}>
           Listen
         </a>
         <span className="text">{props.phonetic.text}</span>
