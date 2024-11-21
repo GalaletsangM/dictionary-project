19f162fdbfc4ca4a8b880aadfa5891251621d8ed
GalaletsangM <preciousmafoko@gmail.com>
2024-11-21 11:12:04 -0600

    created the photos component and style the app

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <img src="logo.svg"
-          className="App-logo img-fluid"
-          alt="logo"></img>
+          <img
+            src="https://seo-hacker.com/wp-content/uploads/2018/05/Search-Goes-Global_A-Look-Into-Search-Engines-Around-the-World-1024x768.jpg"
+            className="App-logo img-fluid"
+            alt="logo"
+          ></img>
         </header>
         <main>
-          <Dictionary defaultKeyWord="sunset"/>
+          <Dictionary defaultKeyWord="sunset" />
         </main>
         <footer className="app-footer">
           <small>coded by Precious</small>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,19 +2,31 @@ import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyWord);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
     console.log(response.data[0]);
   }
+
+  function handleImageResponse(response){
+    console.log(response.data.photos);
+    setPhotos(response.data.photos);
+  }
+
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let imageApiKey = "tb8746f8a332o55bf03481bf03f90fe4";
+    let imageApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${imageApiKey}`;
+    axios.get(imageApiUrl).then(handleImageResponse);
   }
   function handleSubmit(event) {
     event.preventDefault();
@@ -43,6 +55,7 @@ export default function Dictionary(props) {
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
