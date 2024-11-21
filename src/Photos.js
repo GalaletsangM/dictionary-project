ef2258d7a50215bcceb95ff80d44756e2630eaf0
GalaletsangM <preciousmafoko@gmail.com>
2024-11-21 11:24:53 -0600

    added alt prop on photo.js component

--- src/Photos.js
+++ src/Photos.js
@@ -12,6 +12,7 @@ export default function Photos(props) {
                 <img
                   src={photo.src.landscape}
                   className="img-fluid"
+                  alt="photos"
                 ></img>
               </div>
             );
